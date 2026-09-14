93a8353cf7527ca671e24931a70fc7066f22824e
AmanGrg05 <amangrg039@gmail.com>
2026-09-14 16:08:55 +0545

    Added hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import Header from "./components/header";
+import HeroSection from "./components/heroSection";
 
 const App = () => {
   return (
-    <div>
+    <div className="min-h-screen bg-background flex flex-col w-full overflow-x-hidden">
       <Header />
+      <main className="flex-1 pb-24 md:pb-0">
+        <HeroSection />
+      </main>
     </div>
   );
 };
